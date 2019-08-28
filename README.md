87dee94fffec11eef465b7085247e469036fc806
Hawking <hawkingxp@gmail.com>
2019-08-28 19:51:39 +0800

    提交  Android 热更新 测试成功

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ReactNativeHotupdateDemo
 ReactNativeHotupdateDemo
+
+
+# Window 下打包更新 启动闪退bug 提示找不到index.bundlejs：
+
+原因分析：
+
+因为window下 执行
+pushy bundle --platform android
+并未在./build/intermedia/android/index.bundlejs 下生成这个文件 导致对比版本差异的时候 没有可更新的内容，但是应用更新完后 从更新之后的目录读取却找不到相应的bundlejs文件 导致应用闪退
+
+解决办法：
+
+修改项目node_modules目录下 react-native-update/local-cli/lib/bundle.js文件
+查找 react-native bundle  将多行命令换成一行
---
+++ android/app/src/main/java/com/hotupdate5/MainApplication.java
@@ -0,0 +1,66 @@
+package com.hotupdate5;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
+import com.facebook.react.ReactApplication;
+import cn.reactnative.modules.update.UpdatePackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.List;
+import android.util.Log;
+import cn.reactnative.modules.update.UpdateContext;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected String getJSBundleFile() {
+        String url = null;
+        try
+        {
+          url =  UpdateContext.getBundleUrl(MainApplication.this);
+          if (url != null && !url.isEmpty()){
+                Log.d("ReactNativeHostgetJSBundleFile",url);
+          }
+        }catch (Exception e){
+            
+        }
+        return url;
+    }
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages;
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
